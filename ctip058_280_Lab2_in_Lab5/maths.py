13adce1bb44548888fb4a4462982ff6359b58e09
ctip058 <ctip058@aucklanduni.ac.nz>
2019-09-01 10:51:27 +1200

    step 11

--- ctip058_280_Lab2_in_Lab5/maths.py
+++ ctip058_280_Lab2_in_Lab5/maths.py
@@ -43,3 +43,9 @@ def convert_base(num, n):
 
 def factorial(number):
     """Returns the factorial of a number equal to or greater than 0."""
+    if (number == 0):
+        return 1
+    elif (number == 1):
+        return 1
+    else:
+        return number*factorial(number-1)
